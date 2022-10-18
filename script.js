242f9a812029d4c294912ad7c9cfffb69f422b3d
tascintra <tiago.slv@hotmail.com>
2022-10-18 03:10:08 -0300

    isolated the activateDropMenu in a function

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const dropMenuList = document.querySelectorAll('.drop-menu')
+function initDropMenuList() {
+  const dropMenuList = document.querySelectorAll('.drop-menu')
 
-function activateDropMenu() {
-  this.classList.toggle('ativo')
-  this.nextElementSibling.classList.toggle('ativo')
-}
 
-dropMenuList.forEach((item) => {
-  item.addEventListener('click', activateDropMenu)
-})
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.drop-menu')) {
-  const myDropdown = Array.from(document.querySelectorAll(".nav-menu"));
-
-  myDropdown.forEach((item) => {
-    if (item.classList.contains('ativo')) {
-      item.classList.remove('ativo');
-      item.previousElementSibling.classList.remove('ativo');
-    }
+  
+  function activateDropMenu() {
+    this.classList.toggle('active')
+    this.nextElementSibling.classList.toggle('active')
+  }
+  
+  dropMenuList.forEach((item) => {
+    item.addEventListener('click', activateDropMenu)
   })
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.drop-menu')) {
+    const myDropdown = Array.from(document.querySelectorAll(".nav-menu"));
+  
+    myDropdown.forEach((item) => {
+      if (item.classList.contains('active')) {
+          item.classList.remove('active');
+          item.previousElementSibling.classList.remove('active');
+        }
+      })
+    }
   }
 }
+initDropMenuList()
