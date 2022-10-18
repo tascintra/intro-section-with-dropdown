c49a58544823dd910514689210891966221df69e
tascintra <tiago.slv@hotmail.com>
2022-10-18 00:48:08 -0300

    fix: changed for to foreach on window.onlick function

--- script.js
+++ script.js
@@ -9,29 +9,16 @@ dropMenuList.forEach((item) => {
   item.addEventListener('click', activateDropMenu)
 })
 
-const myDropdown = document.getElementsByClassName('nav-menu')
-
-// window.onclick = dropMenuList.forEach((item) => {
-//   if (!item.target.matches('.drop-menu')) {
-//     const myDropdown = document.getElementsByClassName('nav-menu')
-//     if (myDropdown.classList.contains('ativo')) {
-//       myDropdown.classList.remove('ativo');
-//       myDropdown.previousElementSibling.classList.remove('ativo');
-//     }
-//   }
-// })
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.drop-menu')) {
-  var myDropdown = document.getElementsByClassName("nav-menu");
-  for (i = 0; i < 2; i++) {
-    if (myDropdown[i].classList.contains('ativo')) {
-      myDropdown[i].classList.remove('ativo');
-      myDropdown[i].previousElementSibling.classList.remove('ativo');
-    }
-  }
+  const myDropdown = Array.from(document.querySelectorAll(".nav-menu"));
 
+  myDropdown.forEach((item) => {
+    if (item.classList.contains('ativo')) {
+      item.classList.remove('ativo');
+      item.previousElementSibling.classList.remove('ativo');
+    }
+  })
   }
 }
-
-const arrowDown = document.querySelectorAll('.arrow-down')
