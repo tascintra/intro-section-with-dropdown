115f2a0eb505a94916b68fda8fb150f93a85563f
tascintra <tiago.slv@hotmail.com>
2022-10-18 13:55:56 -0300

    fix: dropdown rotation fixed

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 function initDropMenuList() {
-  const dropMenuList = document.querySelectorAll('.drop-menu')
+  const dropMenuList = document.querySelectorAll('.drop-menu');
 
-
-  
   function activateDropMenu() {
-    this.classList.toggle('active')
-    this.nextElementSibling.classList.toggle('active')
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
   }
-  
+
   dropMenuList.forEach((item) => {
-    item.addEventListener('click', activateDropMenu)
-  })
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
+    item.addEventListener('click', activateDropMenu);
+  });
+
+  // Close the dropdown if there's a click outside of it
+  window.onclick = function (e) {
     if (!e.target.matches('.drop-menu')) {
-    const myDropdown = Array.from(document.querySelectorAll(".nav-menu"));
-  
-    myDropdown.forEach((item) => {
-      if (item.classList.contains('active')) {
+      const myDropdown = Array.from(document.querySelectorAll('.nav-menu'));
+
+      myDropdown.forEach((item) => {
+        if (item.classList.contains('active')) {
           item.classList.remove('active');
           item.previousElementSibling.classList.remove('active');
         }
-      })
+      });
     }
-  }
+  };
 }
-initDropMenuList()
+initDropMenuList();
